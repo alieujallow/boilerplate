761ccbc473719680fe50a3ab161664757b767843
alieu <alieujallow93@gmail.com>
2017-10-20 20:08:50 +0000

    map functions added

--- js/app/app.js
+++ js/app/app.js
@@ -17,8 +17,8 @@ var id = 1;
 {
      getMapLocation();
 }*/
- $('#locationBtn').text('Show Location');
- 
+ $('#locationBtn').text('Show');
+
 //gets the location of the device when the button is clicked
  $('#locationBtn').on('click', function()
  {
